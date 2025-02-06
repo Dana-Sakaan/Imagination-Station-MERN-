30a4d91526ab2711ae18e6d9931f0c7b4a4ba459
Dana <danasakaan4@gmail.com>
2025-02-06 13:06:29 +0200

    edit navbar responsivness

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,86 +5,94 @@ import { CiMenuBurger } from "react-icons/ci";
 import { useState } from "react";
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleNav = ()=>{
-    setIsOpen(!isOpen)
-  }
-
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div>
-    <nav className="bg-color1 text-color2 w-[100%]">
-      {/* top navbar */}
-      <div className="flex justify-between p-4 xs:p-8 xs:pb-3 xs:pt-5 ss:justify-around">
-        <div>
-          <Link to="/" className="flex">
-            <img className="w-11 xs:w-14" src={logo} alt="" />
-            <p className="text-color2 mt-3 ml-2 text-lg">Imagination Station</p>
+      <nav className="bg-color1 text-color2 w-[100%]">
+        {/* top navbar */}
+        <div className="flex justify-between p-4 xs:p-8 xs:pb-3 xs:pt-5 ss:justify-around">
+          <div>
+            <Link to="/" className="flex">
+              <img className="w-11 xs:w-14" src={logo} alt="" />
+              <p className="text-color2 mt-3 ml-2 text-lg">
+                Imagination Station
+              </p>
+            </Link>
+          </div>
+          <div className="hidden ss:flex ss:justify-center ss:w-[50%] ss:gap-1">
+            <input
+              className=" w-[70%] text-black rounded-lg p-1"
+              type="text"
+              placeholder="Search..."
+            />
+            <button className="text-xl">
+              <FaSearch />
+            </button>
+          </div>
+          <div className=" flex gap-3 text-xl pt-2 xs:pt-3 xs:text-1xl xs:gap-5 md:text-2xl">
+            <Link to="/cart">
+              <FaShoppingCart className="hover:text-color4" />
+            </Link>
+            <Link to="/wishlist">
+              <FaHeart className="hover:text-color4" />
+            </Link>
+            <Link to="/profile">
+              <FaUser className="hover:text-color4" />
+            </Link>
+            <button onClick={toggleNav} className="xs:hidden">
+              <CiMenuBurger />
+            </button>
+            {/*mobile button*/}
+
+            {/*mobile nav */}
+            <div
+              className={`${
+                isOpen
+                  ? "flex flex-col flex-wrap text-xl xs:hidden"
+                  : "none xs:hidden"
+              }`}
+            >
+              <Link to="/" className="block hover:text-color4">
+                Home
+              </Link>
+              <Link to="/products" className="block hover:text-color4">
+                Products
+              </Link>
+              <Link to="/about" className="block hover:text-color4">
+                About Us
+              </Link>
+
+              <Link to="/signup" className="block hover:text-color4">
+                Create Account
+              </Link>
+            </div>
+            {/*mobile nav*/}
+          </div>
+        </div>
+        <hr />
+
+        {/* Buttom navbar */}
+        <div className="hidden xs:flex xs:justify-around xs:gap-3 xs:p-6 xs:pt-0 xs:pb-2 xs:text-xl ss:text-1xl">
+          <Link to="/" className="hover:text-color4">
+            Home
+          </Link>
+          <Link to="/products" className="hover:text-color4">
+            Products
+          </Link>
+          <Link to="/about" className="hover:text-color4">
+            About Us
+          </Link>
+
+          <Link to="/signup" className="hover:text-color4">
+            Create Account
           </Link>
         </div>
-        <div className="hidden ss:flex ss:justify-center ss:w-[50%] ss:gap-1">
-          <input
-            className=" w-[70%] text-black rounded-lg p-1"
-            type="text"
-            placeholder="Search..."
-          />
-          <button className="text-xl">
-            <FaSearch />
-          </button>
-        </div>
-        <div className=" flex gap-3 text-xl pt-2 xs:pt-3 xs:text-1xl xs:gap-5 md:text-2xl">
-          
-          <Link to="/cart">
-            <FaShoppingCart className="hover:text-color4"/>
-          </Link>
-          <Link to="/wishlist">
-            <FaHeart className="hover:text-color4"/>
-          </Link>
-          <Link to="/profile">
-            <FaUser className="hover:text-color4"/>
-          </Link>
-          <button onClick={toggleNav} className="xs:hidden"><CiMenuBurger className="hover:text-color4"/></button>{/*mobile button*/}
-
-          {/*below the mobile nav */}
-          <div className={`${!isOpen? "none" : "flex flex-col flex-wrap text-xl xs:hidden"}`}>
-        <Link to="/" className="block hover:text-color4">
-          Home
-        </Link>
-        <Link to="/products" className="block hover:text-color4">
-          Products
-        </Link>
-        <Link to="/about" className="block hover:text-color4">
-          About Us
-        </Link>
-
-        <Link to="/signup" className="block hover:text-color4">
-          Create Account
-        </Link>
-      </div>
-      {/*above the mobile nav*/}
-        </div>
-      </div>
-      <hr />
-
-      {/* Buttom navbar */}
-      <div className="hidden xs:flex xs:justify-around xs:gap-3 xs:p-6 xs:pt-0 xs:pb-2 xs:text-xl ss:text-1xl">
-        <Link to="/" className="hover:text-color4">
-          Home
-        </Link>
-        <Link to="/products" className="hover:text-color4">
-          Products
-        </Link>
-        <Link to="/about" className="hover:text-color4">
-          About Us
-        </Link>
-
-        <Link to="/signup" className="hover:text-color4">
-          Create Account
-        </Link>
-      </div>
-    </nav>
-    
+      </nav>
     </div>
   );
 }
